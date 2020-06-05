9e1ae70e07ae0c9df9973cfa6f23028209ccf40d
muguilin <muguilin@foxmail.com>
2020-06-05 18:21:16 +0800

    up nav.ts

--- my-web/src/app/modules/nav/nav.navdata.ts
+++ my-web/src/app/modules/nav/nav.navdata.ts
@@ -2,285 +2,789 @@ export default [
     {
         id: 1,
         pid: 0,
-        name: 'JS库/框架',
+        name: '搜索引擎',
         href: '',
         icon: ''
     },
     {
         id: 2,
         pid: 0,
-        name: 'UI/UE库',
+        name: '党政军事',
         href: '',
         icon: ''
     },
     {
         id: 3,
         pid: 0,
-        name: '字体/图标',
+        name: '行业资讯',
         href: '',
         icon: ''
     },
     {
         id: 4,
         pid: 0,
-        name: '后端/数据库',
+        name: '技术社区',
         href: '',
         icon: ''
     },
     {
         id: 5,
         pid: 0,
-        name: 'CND静态资源',
+        name: '前端团队',
         href: '',
         icon: ''
     },
     {
         id: 6,
         pid: 0,
-        name: '开发工具',
+        name: '学习培训',
         href: '',
         icon: ''
     },
     {
         id: 7,
         pid: 0,
-        name: '社区/博客',
+        name: '文档手册',
         href: '',
         icon: ''
     },
     {
-        id: 1001,
-        pid: 1,
-        name: 'TypeScript',
-        href: '//www.tslang.cn',
+        id: 8,
+        pid: 0,
+        name: '设计/素材',
+        href: '',
+        icon: ''
+    }, {
+        id: 9,
+        pid: 0,
+        name: 'JS类库',
+        href: '',
         icon: ''
     },
     {
-        id: 1002,
-        pid: 1,
-        name: 'Angular.js',
-        href: '//angular.cn',
-        icon: ''
-    }, 
-    {
-        id: 1003,
-        pid: 1,
-        name: 'React.js',
-        href: '//zh-hans.reactjs.org',
-        icon: ''
-    }, 
-    {
-        id: 1004,
-        pid: 1,
-        name: 'Vue.js',
-        href: '//cn.vuejs.org',
-        icon: ''
-    },
-    
-    {
-        id: 1005,
-        pid: 1,
-        name: 'jQuery.js',
-        href: '//jquery.com',
+        id: 10,
+        pid: 0,
+        name: '字体/图标',
+        href: '',
         icon: ''
     },
     {
-        id: 1006,
-        pid: 1,
-        name: 'Zeptojs.js',
-        href: '//zeptojs.com',
+        id: 11,
+        pid: 0,
+        name: 'UI/UE库',
+        href: '',
         icon: ''
     },
     {
-        id: 2001,
+        id: 12,
+        pid: 0,
+        name: 'JS框架',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 13,
+        pid: 0,
+        name: '构建/调试',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 14,
+        pid: 0,
+        name: 'CMS/模板',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 15,
+        pid: 0,
+        name: 'CND资源',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 16,
+        pid: 0,
+        name: '开放平台',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 17,
+        pid: 0,
+        name: 'SDK/API',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 18,
+        pid: 0,
+        name: '后端开发',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 19,
+        pid: 0,
+        name: '数据库',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 20,
+        pid: 0,
+        name: '云端服务',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 21,
+        pid: 0,
+        name: '优化/测试',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 22,
+        pid: 0,
+        name: '网络运维',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 23,
+        pid: 0,
+        name: '大数据',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 24,
+        pid: 0,
+        name: '人工知能',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 25,
+        pid: 0,
+        name: '机器学习',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 26,
+        pid: 0,
+        name: '开发环境',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 27,
+        pid: 0,
+        name: '工作软件',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 28,
+        pid: 0,
+        name: '实用工具',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 29,
+        pid: 0,
+        name: '考试学历',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 30,
+        pid: 0,
+        name: '招聘求职',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 31,
+        pid: 0,
+        name: '知名博客',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 32,
+        pid: 0,
+        name: '生活百科',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 33,
+        pid: 0,
+        name: '休闲娱乐',
+        href: '',
+        icon: ''
+    },
+    {
+        id: 100001,
+        pid: 1,
+        name: 'Google',
+        href: 'https://www.google.com',
+        icon: ''
+    },
+    {
+        id: 100002,
+        pid: 1,
+        name: '百度',
+        href: '//www.baidu.com',
+        icon: ''
+    },
+    {
+        id: 100003,
+        pid: 1,
+        name: 'Bing',
+        href: '//cn.bing.com',
+        icon: ''
+    },
+    {
+        id: 100004,
+        pid: 1,
+        name: 'Yahoo',
+        href: '//search.yahoo.com',
+        icon: ''
+    },
+    {
+        id: 100005,
+        pid: 1,
+        name: '360搜索',
+        href: '//www.so.com',
+        icon: ''
+    },
+    {
+        id: 100006,
+        pid: 1,
+        name: 'China搜索',
+        href: '//www.chinaso.com',
+        icon: ''
+    },
+    {
+        id: 100007,
+        pid: 1,
+        name: 'Sogou搜索',
+        href: '//www.sogou.com',
+        icon: ''
+    },
+    {
+        id: 200001,
         pid: 2,
-        name: 'Bootstrap',
-        href: '//www.bootcss.com',
+        name: '中华网',
+        href: '//www.china.com',
+        icon: ''
+    },
+    {
+        id: 200002,
+        pid: 2,
+        name: '中国网',
+        href: '//www.china.com.cn',
+        icon: ''
+    },
+    {
+        id: 200003,
+        pid: 2,
+        name: '人民网',
+        href: '//people.com.cn',
+        icon: ''
+    },
+    {
+        id: 200004,
+        pid: 2,
+        name: '光明网',
+        href: '//www.gmw.cn',
+        icon: ''
+    },
+    {
+        id: 200005,
+        pid: 2,
+        name: '党员网',
+        href: '//www.12371.cn',
+        icon: ''
+    },
+    {
+        id: 200005,
+        pid: 2,
+        name: '求是网',
+        href: '//www.qstheory.cn',
+        icon: ''
+    },
+    {
+        id: 200006,
+        pid: 2,
+        name: '铁血网',
+        href: '//www.tiexue.net',
+        icon: ''
+    },
+    {
+        id: 200007,
+        pid: 2,
+        name: '央视网',
+        href: '//www.cctv.com',
+        icon: ''
+    },
+    {
+        id: 200008,
+        pid: 2,
+        name: '环球网',
+        href: '//www.huanqiu.com',
+        icon: ''
+    },
+    {
+        id: 200009,
+        pid: 2,
+        name: '中纪委',
+        href: '//www.ccdi.gov.cn',
+        icon: ''
+    },
+    {
+        id: 200010,
+        pid: 2,
+        name: '国防部',
+        href: '//www.mod.gov.cn',
+        icon: ''
+    },
+    {
+        id: 200011,
+        pid: 2,
+        name: '外交部',
+        href: '//www.fmprc.gov.cn',
+        icon: ''
+    },
+    {
+        id: 200012,
+        pid: 2,
+        name: '学习强国',
+        href: '//www.xuexi.cn',
+        icon: ''
+    },
+    {
+        id: 200013,
+        pid: 2,
+        name: '中国军网',
+        href: '//www.81.cn',
+        icon: ''
+    },
+    {
+        id: 200014,
+        pid: 2,
+        name: '国际在线',
+        href: '//www.cri.cn',
+        icon: ''
+    },
+    {
+        id: 200015,
+        pid: 2,
+        name: '中国政府网',
+        href: '//www.gov.cn',
+        icon: ''
+    },
+    {
+        id: 200016,
+        pid: 2,
+        name: '中国新闻网',
+        href: '//www.chinanews.com',
+        icon: ''
+    },
+    {
+        id: 200017,
+        pid: 2,
+        name: '中国经济网',
+        href: '//www.ce.cn',
+        icon: ''
+    },
+    {
+        id: 200018,
+        pid: 2,
+        name: '中国科学网',
+        href: '//www.minimouse.com.cn',
+        icon: ''
+    },
+    {
+        id: 200019,
+        pid: 2,
+        name: '中国法律网',
+        href: '//www.5law.cn',
+        icon: ''
+    },
+    {
+        id: 200020,
+        pid: 2,
+        name: '中国青年网',
+        href: '//www.youth.cn',
+        icon: ''
+    },
+    {
+        id: 200021,
+        pid: 2,
+        name: '国家铁路局',
+        href: '//www.nra.gov.cn',
+        icon: ''
+    },
+    {
+        id: 200022,
+        pid: 2,
+        name: '国家信访局',
+        href: '//www.gjxfj.gov.cn',
+        icon: ''
+    },
+    {
+        id: 200023,
+        pid: 2,
+        name: '国家税务总局',
+        href: '//www.chinatax.gov.cn',
+        icon: ''
+    },
+
+    {
+        id: 200024,
+        pid: 2,
+        name: '国家体育总局',
+        href: '//www.sport.gov.cn',
+        icon: ''
+    },
+    {
+        id: 200025,
+        pid: 2,
+        name: '国家知识产权局',
+        href: '//www.cnipa.gov.cn',
+        icon: ''
+    },
+    {
+        id: 200026,
+        pid: 2,
+        name: '中国科学技术部',
+        href: '//www.most.gov.cn',
+        icon: ''
+    },
+    {
+        id: 200027,
+        pid: 2,
+        name: '中国网络安全网',
+        href: '//www.cac.gov.cn',
         icon: ''
     }, 
     {
-        id: 2002,
-        pid: 2,
-        name: 'SemanticUI',
-        href: '//semantic-ui.com',
-        icon: ''
-    }, 
-    {
-        id: 2003,
-        pid: 2,
-        name: 'AmazeUI',
-        href: '//amazeui.shopxo.net',
-        icon: ''
-    },   
-    {
-        id: 2004,
-        pid: 2,
-        name: 'Layui',
-        href: '//www.layui.com',
-        icon: ''
-    }, 
-    {
-        id: 2005,
-        pid: 2,
-        name: 'ElementUI',
-        href: '//element.eleme.cn',
-        icon: ''
-    }, 
-    {
-        id: 2006,
-        pid: 2,
-        name: 'AntDesign',
-        href: '//ant.design/index-cn',
-        icon: ''
-    }, 
-    {
-        id: 3001,
+        id: 300001,
         pid: 3,
-        name: 'FontAwesome',
-        href: '//www.fontawesome.com.cn',
+        name: '极客公园',
+        href: '//www.geekpark.net',
         icon: ''
     },
     {
-        id: 3002,
-        pid: 3,
-        name: 'Iconfont-阿里巴巴矢量图标库',
-        href: '//www.iconfont.cn',
-        icon: ''
-    },
-    {
-        id: 3003,
-        pid: 3,
-        name: 'IcoMoon',
-        href: '//icomoon.io',
-        icon: ''
-    },
-    {
-        id: 3004,
-        pid: 3,
-        name: '图标下载',
-        href: '//www.easyicon.net',
-        icon: ''
-    }, 
-    {
-        id: 3004,
-        pid: 3,
-        name: 'ICO图标生成',
-        href: '//www.ico51.cn',
-        icon: ''
-    },
-    {
-        id: 4001,
+        id: 400001,
         pid: 4,
-        name: 'Node.js',
-        href: '//nodejs.cn',
-        icon: ''
-    },
-    {
-        id: 4002,
-        pid: 4,
-        name: 'MongoDB',
-        href: '//www.mongodb.com',
-        icon: ''
-    },
-    {
-        id: 5001,
-        pid: 5,
-        name: 'BootCDN',
-        href: '//www.bootcdn.cn',
-        icon: ''
-    },
-    {
-        id: 6001,
-        pid: 6,
-        name: '图片压缩',
-        href: '//tinypng.com',
-        icon: ''
-    },
-    {
-        id: 6002,
-        pid: 6,
-        name: '在线工具',
-        href: '//tool.lu',
-        icon: 'book'
-    },
-    {
-        id: 6003,
-        pid: 6,
-        name: '在线工具',
-        href: '//tool.oschina.net',
-        icon: 'book'
-    },
-    {
-        id: 6004,
-        pid: 6,
-        name: 'JSON',
-        href: '//json.cn',
-        icon: 'book'
-    },
-    {
-        id: 7001,
-        pid: 7,
-        name: 'MDN Web Docs',
-        href: '//developer.mozilla.org/zh-CN',
-        icon: ''
-    },
-    {
-        id: 7002,
-        pid: 7,
-        name: '菜鸟教程',
-        href: '//www.runoob.com',
-        icon: ''
-    },
-    {
-        id: 7003,
-        pid: 7,
         name: 'CSDN',
         href: '//blog.csdn.net',
         icon: ''
     },
     {
-        id: 7004,
-        pid: 7,
-        name: '开源中国',
-        href: '//www.oschina.net',
-        icon: ''
-    },
-    {
-        id: 7005,
-        pid: 7,
+        id: 400002,
+        pid: 4,
         name: '51CTO',
         href: '//www.51cto.com',
         icon: ''
     },
     {
-        id: 7006,
+        id: 400003,
+        pid: 4,
+        name: '开源中国',
+        href: '//www.oschina.net',
+        icon: ''
+    },
+    {
+        id: 500001,
+        pid: 5,
+        name: '淘系前端团队',
+        href: '//fed.taobao.org',
+        icon: ''
+    },
+    {
+        id: 600001,
+        pid: 6,
+        name: '菜鸟教程',
+        href: '//www.runoob.com',
+        icon: ''
+    },
+    {
+        id: 700001,
+        pid: 7,
+        name: 'MDN Web Docs',
+        href: '//developer.mozilla.org/zh-CN',
+        icon: ''
+    },
+
+    {
+        id: 700002,
         pid: 7,
         name: 'W3CSHOOL',
         href: '//www.w3cschool.cn',
         icon: ''
     },
     {
-        id: 7007,
+        id: 700003,
         pid: 7,
         name: 'W3CSHOOLCN',
         href: '//www.w3school.com.cn',
         icon: ''
     },
     {
-        id: 7008,
+        id: 700004,
         pid: 7,
         name: 'W3CPLUS',
         href: '//www.w3cplus.com',
         icon: ''
     },
     {
-        id: 7009,
+        id: 700005,
         pid: 7,
         name: 'W3CHTML',
         href: '//www.w3chtml.com',
         icon: ''
-    }
+    },
+    {
+        id: 800001,
+        pid: 8,
+        name: 'UI中国',
+        href: '//www.ui.cn',
+        icon: ''
+    },
+    {
+        id: 800002,
+        pid: 8,
+        name: '最美UI',
+        href: '//www.zuimeiui.cn',
+        icon: ''
+    },
+    {
+        id: 800003,
+        pid: 8,
+        name: 'UIGreat',
+        href: '//uigreat.com',
+        icon: ''
+    },
+    {
+        id: 900001,
+        pid: 9,
+        name: 'jQuery.js',
+        href: '//jquery.com',
+        icon: ''
+    },
+    {
+        id: 900002,
+        pid: 9,
+        name: 'Zeptojs.js',
+        href: '//zeptojs.com',
+        icon: ''
+    },
+    {
+        id: 100001,
+        pid: 10,
+        name: 'FontAwesome',
+        href: '//www.fontawesome.com.cn',
+        icon: ''
+    },
+    {
+        id: 100002,
+        pid: 10,
+        name: 'Iconfont',
+        href: '//www.iconfont.cn',
+        icon: ''
+    },
+    {
+        id: 100003,
+        pid: 10,
+        name: 'IcoMoon',
+        href: '//icomoon.io',
+        icon: ''
+    },
+    {
+        id: 100004,
+        pid: 10,
+        name: 'Easyicon',
+        href: '//www.easyicon.net',
+        icon: ''
+    },
+    {
+        id: 100005,
+        pid: 10,
+        name: 'Icons8',
+        href: '//icons8.cn',
+        icon: ''
+    },
+    {
+        id: 100006,
+        pid: 10,
+        name: 'IconStore',
+        href: '//iconstore.co',
+        icon: ''
+    },
+    {
+        id: 100007,
+        pid: 10,
+        name: 'IconNinja',
+        href: '//www.iconninja.com',
+        icon: ''
+    },
+    {
+        id: 100008,
+        pid: 10,
+        name: 'ICO图标生成',
+        href: '//www.ico51.cn',
+        icon: ''
+    },
+    {
+        id: 110001,
+        pid: 11,
+        name: 'Bootstrap',
+        href: '//www.bootcss.com',
+        icon: ''
+    },
+    {
+        id: 110002,
+        pid: 11,
+        name: 'SemanticUI',
+        href: '//semantic-ui.com',
+        icon: ''
+    },
+    {
+        id: 110003,
+        pid: 11,
+        name: 'AmazeUI',
+        href: '//amazeui.shopxo.net',
+        icon: ''
+    },
+    {
+        id: 110004,
+        pid: 11,
+        name: 'Layui',
+        href: '//www.layui.com',
+        icon: ''
+    },
+    {
+        id: 110005,
+        pid: 11,
+        name: 'ElementUI',
+        href: '//element.eleme.cn',
+        icon: ''
+    },
+    {
+        id: 110006,
+        pid: 11,
+        name: 'AntDesign',
+        href: '//ant.design/index-cn',
+        icon: ''
+    },
+    {
+        id: 120001,
+        pid: 12,
+        name: 'Angular.js',
+        href: '//angular.cn',
+        icon: ''
+    },
+    {
+        id: 120002,
+        pid: 12,
+        name: 'React.js',
+        href: '//zh-hans.reactjs.org',
+        icon: ''
+    },
+    {
+        id: 120003,
+        pid: 12,
+        name: 'Vue.js',
+        href: '//cn.vuejs.org',
+        icon: ''
+    },
+    {
+        id: 130001,
+        pid: 13,
+        name: 'TypeScript',
+        href: '//www.tslang.cn',
+        icon: ''
+    },
 
+    {
+        id: 150001,
+        pid: 15,
+        name: 'BootCDN',
+        href: '//www.bootcdn.cn',
+        icon: ''
+    },
+    {
+        id: 180001,
+        pid: 18,
+        name: 'Node.js',
+        href: '//nodejs.cn',
+        icon: ''
+    },
+    {
+        id: 190001,
+        pid: 19,
+        name: 'MongoDB',
+        href: '//www.mongodb.com',
+        icon: ''
+    },
+    {
+        id: 200001,
+        pid: 20,
+        name: '阿里云',
+        href: '//www.aliyun.com',
+        icon: ''
+    },
+    {
+        id: 200002,
+        pid: 20,
+        name: '腾讯云',
+        href: '//cloud.tencent.com',
+        icon: ''
+    },
+    {
+        id: 200003,
+        pid: 20,
+        name: '百度云',
+        href: '//cloud.baidu.com',
+        icon: ''
+    },
+    {
+        id: 280001,
+        pid: 28,
+        name: '图片压缩',
+        href: '//tinypng.com',
+        icon: ''
+    },
+    {
+        id: 280002,
+        pid: 28,
+        name: '在线工具',
+        href: '//tool.lu',
+        icon: 'book'
+    },
+    {
+        id: 280003,
+        pid: 28,
+        name: '在线工具',
+        href: '//tool.oschina.net',
+        icon: 'book'
+    },
+    {
+        id: 280004,
+        pid: 28,
+        name: 'JSON',
+        href: '//json.cn',
+        icon: 'book'
+    },
 
 ];
 
